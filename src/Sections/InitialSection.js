ea67124dc9aef141b8979b41a696c1dbb2c2aeea
Luann Sapucaia <sapucaialuann@id.uff.br>
2021-07-22 23:42:26 -0300

    added libraries to accelerate the development process

---
+++ src/Sections/InitialSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import '../Styles/InitialSection.css';
+
+export default function InitialSection() {
+  return(
+    <div className="homeContainer">
+    </div>
+  )
+}
