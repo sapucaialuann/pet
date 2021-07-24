32a1bd2c44c92bdf4ac97323961681db359a9e60
Luann <luann.sapucaia@championx.com>
2021-07-24 20:42:31 -0300

    Added handlerMethod in Home page, created a switch case inside of it

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,9 +2,27 @@ import Header from '../Components/Header';
 import InitialSection from '../Sections/InitialSection';
 import '../Styles/Home.css';
 
-
-function Adoption() {
-  
+//here this function will be responsible for calling the other react components. Each card will have a value that will be passed as a parameter to the main function.
+//Below the  card, it will be added a button that will respond to the component handled by the HandleEvents Function.
+function HandleEvents(value) {
+  switch (value) {
+    case 1: 
+      // return <Walk />
+      break;
+    case 2: 
+      // return <Adoption />
+      break;
+    case 3: 
+      // return <Store />
+      break;
+    case 4: 
+      // return <Reservation />
+      break;
+    case 5: 
+      // return <AdoptAnimal />
+      break;
+      
+  }
 }
 
 
