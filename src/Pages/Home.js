30816a601fcf7afdabc8a992a5b93c950a77762b
Luann <luann.sapucaia@championx.com>
2021-07-24 19:30:58 -0300

    Changed the home structure to something easier to develop

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,12 @@
 import Header from '../Components/Header';
 import InitialSection from '../Sections/InitialSection';
 import '../Styles/Home.css';
-import { Button, Card } from 'react-bootstrap';
-import dog from '../Images/dog.png';
-import health from '../Images/health.png';
-import petbed from '../Images/petbed.png';
-import petfriendly from '../Images/petfriendly.png';
-import pethouse from '../Images/pethouse.png';
 
 export default function Home() {
   return (
     <>
       <Header />
       <InitialSection />
-      <div className='middleSection'>
-        <Card>
-          <Card.Img variant="top" src={dog} />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk of
-              the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
     </>
   );
 }
--- src/Sections/InitialSection.js
+++ src/Sections/InitialSection.js
@@ -1,10 +1,79 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
 import '../Styles/InitialSection.css';
+import { Button, Card, CardGroup } from 'react-bootstrap';
+import dog from '../Images/dog.png';
+import health from '../Images/health.png';
+import petbed from '../Images/petbed.png';
+import petfriendly from '../Images/petfriendly.png';
+import pethouse from '../Images/pethouse.png';
+
 
 export default function InitialSection() {
   return(
-    <div className="homeContainer">
+    <div className="homeContainer middleSection">
+      <CardGroup className="cardGroupMiddle" style={{width: '80rem'}}>
+          <Card >
+            <Card.Img variant="top" src={dog} />
+            <Card.Body>
+            <Button variant="primary">
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card >
+            <Card.Img variant="top" src={health} />
+            <Card.Body>
+            <Button variant="danger">
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card >
+            <Card.Img variant="top" src={petbed} />
+            <Card.Body>
+            <Button variant="success">
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card >
+            <Card.Img variant="top" src={petfriendly} />
+            <Card.Body>
+            <Button variant="warning">
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              </Button>
+            </Card.Body>
+          </Card>
+          <Card >
+            <Card.Img variant="top" src={pethouse} />
+            <Card.Body>
+            <Button variant="info">
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              </Button>
+            </Card.Body>
+          </Card>
+        </CardGroup>
     </div>
   )
 }
