654dbaa0a434cad1929bae87b5244659dda24bf4
Luann <luann.sapucaia@championx.com>
2021-07-26 22:28:15 -0300

    Added firebase libraries and firebase service to the project. In the GenericCardComponent.js it's been created a logic in the attempt to make the connection with the firebase work and get the info from the DB

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home';
 import Routes from './Routes/Index';
+import { FirebaseDatabaseProvider } from '@react-firebase/database';
 
 function App() {
   return (
-    <>
+    <FirebaseDatabaseProvider>
       <Routes />
-    </>
+    </FirebaseDatabaseProvider>
   );
 }
 
