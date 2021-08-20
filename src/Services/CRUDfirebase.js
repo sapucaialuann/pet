d82e5e1918bad9129bd6af7439889ed0d372579a
Luann <luann.sapucaia@championx.com>
2021-08-20 19:29:10 -0300

    added all the references in the crud file

--- src/Services/CRUDfirebase.js
+++ src/Services/CRUDfirebase.js
@@ -1,7 +1,18 @@
 import firebase from "./FirebaseConfig";
 
 
-const db = firebase.ref("/passeio");
+const db = firebase.ref("/hotel");
+// const db = firebase.ref("/cliente");
+// const db = firebase.ref("/denuncia");
+// const db = firebase.ref("/funcionario");
+// const db = firebase.ref("/avaliacao");
+// const db = firebase.ref("/passeio");
+// const db = firebase.ref("/pet");
+// const db = firebase.ref("/produto");
+// const db = firebase.ref("/reserva");
+// const db = firebase.ref("/venda");
+
+
 
 const getAll = () => {
     return db;
